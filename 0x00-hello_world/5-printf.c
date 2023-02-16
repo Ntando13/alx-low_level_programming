7f96b39eabb0f64a8d1a117d1f9d6d521ae6810b
Ntando13 <3170880@myuwc.ac.za>
2023-02-15 23:19:23 -0800

    5-printf.c

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+/**
+ * Description: main - Program that prints aline using  puts.
+ * Return: 0 if success.
+ */
+int main(void)
+{
+printf("with proper grammar, but the outcome is a piece of art,\n");
+return (0);
+}
