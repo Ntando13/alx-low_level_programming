07e36f93472f31a9a57e2c9e111941b555272ad1
Ntando13 <3170880@myuwc.ac.za>
2023-02-16 00:07:08 -0800

    101-quote.c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,11 +1,13 @@
 #include <stdio.h>
 #include <unistd.h>
 /**
- * Descrption: main A c program that prints a line
- *Return: Always 1 (Success)
+ *  main - Entry point
+ *Return: Always 0 (Success)
  */
 int main(void)
 {
-	fput("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", stdout);
+write(1, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n",
+write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n",
+sizeof("and that piece of art is useful\" - Dora Korpar, 2015-10-19"));
 return (1);
 }
