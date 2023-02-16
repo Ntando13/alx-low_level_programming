bee3727d1c9669d420ac504617a18eb181fe5edb
Ntando13 <3170880@myuwc.ac.za>
2023-02-15 22:58:48 -0800

    4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,2 +1,11 @@
-#!/bin/bash
-
+#include <stdio.h>
+/**
+ * Description: main -  Program that printa line with puts
+ *
+ * Return: 0 (Success)
+ */
+int main(void)
+{
+puts("\"Programming is like building a multilingual puzzle");
+return (0);
+}
