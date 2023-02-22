ef30e84fab6915b6be33cf48e1dc210895ab711f
Ntando13 <3170880@myuwc.ac.za>
2023-02-22 13:02:02 -0800

    2-largest_number.c

--- 0x03-debugging/main.h
+++ 0x03-debugging/main.h
@@ -5,3 +5,5 @@ void positive_or_negative(int i);
 void largest_number(int a, inday);
 void convert_day(int month, int day);
 void print_remaining_days(int month, int day, int year);
+
+#endif /* MAIN_H */
