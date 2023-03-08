72daa3f01105572abf9a77e1314cbb3d204723f2
Ntando13 <3170880@myuwc.ac.za>
2023-03-08 09:03:55 -0800

    100-is_palindrome.c

--- 0x08-recursion/100-is_palindrome.c
+++ 0x08-recursion/100-is_palindrome.c
@@ -1,29 +1,44 @@
+#include <stdio.h>
 #include "main.h"
 
 /**
- * wildcmp - compares two strings and returns 1 if the strings
- * can be considered identical, otherwise return 0.
- * @s1: string to compare to
- * @s2: string with wild character
- *
- * Return: On success 1.
- * On error, -1 is returned, and errno is set appropriately.
+ * _strlen_recursion - returns the length of a string.
+ * @s: the string to count
+ * Return: length of the string
  */
-int wildcmp(char *s1, char *s2)
+int _strlen_recursion(char *s)
 {
-	if (*s1 == '\0' && *s2 == '\0')
-		return (1);
-
-	if (*s1 == *s2)
-		return (wildcmp(s1 + 1, s2 + 1));
-
-	if (*s2 == '*')
+	if (*s)
 	{
-		if (*s2 == '*' && *(s2 + 1) != '\0' && *s1 == '\0')
-			return (0);
-		if (wildcmp(s1, s2 + 1) || wildcmp(s1 + 1, s2))
-			return (1);
+		s++;
+		return (1 + _strlen_recursion(s));
 	}
-
 	return (0);
+}
+/**
+ * checker - helper function for is_palindrome
+ * @str: the string
+ * @len: length of string
+ * @count: counter of recursion
+ * Return: 1 if string is a palindrome, 0 if it is not.
+ */
+int checker(char *str, int len, int count)
+{
+	if (count >= len)
+		return (1);
+	if (str[len] == str[count])
+		return (checker(str, len - 1, count + 1));
+	return (0);
+}
+/**
+ * is_palindrome - checks if the string is a palindrome
+ * @s: the string to check
+ * Return: 1 if string is a palindrome, 0 if it is not.
+ */
+int is_palindrome(char *s)
+{
+	int len = _strlen_recursion(s);
+	int count = 0;
+
+	return (checker(s, len - 1, count));
 }
