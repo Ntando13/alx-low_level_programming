5046688e502e174a078a1a83a2c60471caa9beb4
Ntando13 <3170880@myuwc.ac.za>
2023-03-30 20:02:20 -0700

    101-hello_holberton.asm

--- 0x12-singly_linked_lists/101-hello_holberton.asm
+++ 0x12-singly_linked_lists/101-hello_holberton.asm
@@ -1,16 +1,14 @@
+global  main
 section .text
-	global main
-
 main:
-	mov	edx, len
-	mov	ecx, msg	;message variable
-	mov 	ebx, 1		;write to standard output
-	mov	eax, 4
-	int	0x80		;call kernel
+        mov     rax, 1
+        mov     rdi, 1
+        mov     rsi, message
+        mov     rdx, 17
+	syscall
 
-	mov	eax, 1 		;system call (sys_exit)
-	int	0x80		;call kernel
-
-section .data
-msg db 'Hello, Coding', 0xa	;message to print
-len equ $ - msg			;length of the string
+        mov     rax, 60
+        xor     rdi, rdi
+	syscall
+message:
+        db      "Hello, Holberton", 10
