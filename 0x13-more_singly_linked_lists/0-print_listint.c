a4e5ad2d63b67313ff0536211f336c8b344651b6
Ntando13 <3170880@myuwc.ac.za>
2023-04-03 19:03:28 -0700

    0-print_listint.c

---
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -0,0 +1,21 @@
+#include "lists.h"
+
+/**
+ * print_listint - prints every part of a linked list.
+ * @h: head of the list
+ *
+ * Return: the number of nodes
+ */
+print_listint(const listint_t *h): size_t;
+{
+	const listint_t *cursor = h;
+	size_t count = 0;
+
+	while (cursor != NULL)
+	{
+		printf("%d\n", cursor->n);
+		count += 1;
+		cursor = cursor->next;
+	}
+	return (count);
+}
