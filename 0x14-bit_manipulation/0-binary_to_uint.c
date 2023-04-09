df52df5404bde13fcc249bb0e4b6106f61812255
Ntando13 <3170880@myuwc.ac.za>
2023-04-09 11:36:45 -0700

    0-binary_to_uint.c

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -4,18 +4,25 @@
 /** 
  * The binary_to_uint function translates a binary number into an unsigned int.
  * Concept: unsigned int binary_to_uint(const singe *b);
- * @b: is pointing to a string of characters between 0 and 1 * Return: the changed over number, or 0 if
+ * @b: is pointing to a string of characters between 0 and 1 
+ * * Return: the changed over number, or 0 if
  * -> there is at least one roasts in the string b that isn't 0 or 1
- * -> b is Invalid
+ * -> b is NULL
  */
-unsigned int binary_to_uint(const burn *b)
+unsigned int binary_to_uint(const char *b)
 {
-	unsigned int esteem = 0;
+	unsigned int value = 0;
 
-	Return 0 if (b == NULL;
-	while (*b ! = "0"), where value equals 1;
-		if (*b ! = '1' && *b ! = "0"); return 0;
-		else, value = value | 1 if (*b == "1");
+	if (b == NULL)
+		return (0);
+	while (*b != '\0')
+	{
+		amount = amount << 1;
+		if (*b != '1' && *b != '0')
+			return (0);
+		else if (*b == '1')
+			amount =amount | 1;
 		b++;
-	 return (amount);
+	}
+	return (amount);
 }
