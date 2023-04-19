59fceedbaf0b917caee698dc71f480347263126a
Ntando13 <3170880@myuwc.ac.za>
2023-04-19 02:23:09 -0700

    2-append_text_to_file.c

--- 0x15-file_io/2-append_text_to_file.c
+++ 0x15-file_io/2-append_text_to_file.c
@@ -1,53 +1,38 @@
-#include <stdio.h>
-#include <stdlib.h>
+/*
+ * File: 1-create_file.c
+ * Auth: Gedeon Obae Gekonge
+ */
+
 #include "main.h"
 
 /**
- * _strlen - compute the length of a NULL-terminated string
- * @str: the string to measure
+ * create_file - Creates a file.
+ * @filename: A pointer to the name of the file to create.
+ * @text_content: A pointer to a string to write to the file.
  *
- * Return: the length of str, or -1 if str is NULL
+ * Return: If the function fails - -1.
+ *         Otherwise - 1.
  */
-ssize_t _strlen(const char *str)
+int create_file(const char *filename, char *text_content)
 {
-	ssize_t len = 0;
+	int o, w, len = 0;
 
-	if (!str)
+	if (filename == NULL)
 		return (-1);
 
-	while (*str++)
-		++len;
+	if (text_content != NULL)
+	{
+		for (len = 0; text_content[len];)
+			len++;
+	}
 
-	return (len);
-}
+	o = open(filename, O_CREAT | O_RDWR | O_TRUNC, 0600);
+	w = write(o, text_content, len);
 
-/**
- * append_text_to_file - append text to the end of a file
- * @filename: the name of the file to append to
- * @text_content: the data to append to filename
- *
- * Return: Upon success, return 1. Otherwise, return -1.
- */
-int append_text_to_file(const char *filename, char *text_content)
-{
-	ssize_t b_written = 0;
-	int fd;
-
-	if (!filename)
+	if (o == -1 || w == -1)
 		return (-1);
 
-	fd = open(filename, O_WRONLY | O_APPEND);
+	close(o);
 
-	if (fd < 0)
-		return (-1);
-
-	if (text_content)
-		b_written = write(fd, text_content, _strlen(text_content));
-
-	close(fd);
-
-	if (b_written < 0)
-		return (-1);
 	return (1);
 }
-
